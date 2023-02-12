4b16280b888633ffdb0760998da06747c34ff1ce
theking250902 <buivanvietx5hongloc@gmail.com>
2023-02-12 14:27:19 +0700

    màn hình Welcome

--- Grocery_app/App.js
+++ Grocery_app/App.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Welcome from './app/Welcome'
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+        <Welcome/>
     </View>
   )
 }
---
+++ Grocery_app/app/Welcome.js
@@ -0,0 +1,69 @@
+import { StyleSheet, Text, View,Image, Pressable } from 'react-native'
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <View style={styles.container}>
+        <Image style={styles.image} source={require('./image/Illustration.png')}/>
+        <Text style={styles.txt_realax}>Realax and shop</Text>
+        <View style={styles.text}>
+            <Text style={styles.text}>Shop online and get grocories</Text>
+            <Text style={styles.text}>delivered from stores to your home</Text>
+            <Text style={styles.text}> in as fast as 1 hour .</Text>
+        </View>
+        <Pressable style={styles.btn_up}>
+            <Text style={styles.txt_btn}>Sign up</Text>
+        </Pressable>
+        <Pressable style={styles.btn_in}>
+            <Text style={{color:'#FF5E00'}}>Sign in</Text>
+        </Pressable>
+    </View>
+  )
+}
+
+export default Welcome
+
+const styles = StyleSheet.create({
+    container:{
+        alignItems:'center'
+    },
+    image:{
+        width:310,
+        height:330,
+        marginTop:40,
+        marginRight:5
+    },
+    txt_realax:{
+        fontSize:20,
+        marginTop:35,
+        color:'#7F4E1D',
+        marginBottom:15
+    },text:{
+        alignItems:'center',
+        color:'#7F4E1D'
+    },
+    btn_up:{
+        backgroundColor:'#FF5E00',
+        borderRadius:30,
+        width:343,
+        height:50,
+        alignItems:'center',
+        flexDirection:'column',
+        justifyContent:'center',
+        marginTop:51
+    }
+    ,txt_btn:{           
+            color:'#FFFFFF'
+    },
+    btn_in:{
+        borderColor:'#FF5E00',
+        borderRadius:30,
+        borderWidth:1,
+        width:343,
+        height:50,
+        alignItems:'center',
+        flexDirection:'column',
+        justifyContent:'center',
+        marginTop:16,
+    }
+})
