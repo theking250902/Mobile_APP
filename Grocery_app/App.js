5f6f27a34d8434f4a4549c0eb3584e29cf2c5f19
Vanilla23 <lamvip06192003@gmail.com>
2023-02-13 23:35:34 +0700

    test git

--- Grocery_app/App.js
+++ Grocery_app/App.js
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {Text, View} from 'react-native';
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>Lamdz</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
