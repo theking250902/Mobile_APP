475d408c98a889e25fa10b40fe9e7d6f78a774bd
HoangMylb <nguyenmylb85@gmail.com>
2023-02-16 14:52:32 +0700

    B

--- Grocery_app/App.js
+++ Grocery_app/App.js
@@ -1,11 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Register1 from './src/screens/Register1'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+   <Register1></Register1>
   )
 }
 
---
+++ Grocery_app/src/screens/Login.js
@@ -0,0 +1,190 @@
+import { StyleSheet, Text, View, Image, TextInput, Pressable, Button } from 'react-native'
+import React from 'react'
+
+const Login = () => {
+    return (
+        <View style={styles.container}>
+
+            <Image source={require('../../image/tenTrai.png')} style={styles.imageTren} ></Image>
+            <Text style={styles.textSI}>Sign In</Text>
+            <Image source={require('../../image/4.png')} style={styles.image4} resizeMode='cover' ></Image>
+            <Text style={styles.detail}>Enter your phone number and
+                password to access your account</Text>
+            <View style={styles.icon} >
+                <Image
+                    source={require('../../image/co.png')}
+                    style={styles.co}
+                    resizeMode='cover'
+                ></Image>
+                <Image
+                    source={require('../../image/tenXuong.png')}
+                    style={styles.tenXuong}
+                    resizeMode='cover' ></Image>
+
+                <TextInput style={styles.numberPhone} placeholder='Number Phone'></TextInput>
+            </View>
+
+            <View style={styles.password}>
+                <TextInput style={styles.inPassword} secureTextEntry={true} placeholder='Password'></TextInput>
+                <Image source={require('../../image/eyes.png')}
+                    style={styles.eyes}
+                    resizeMode='cover'></Image>
+            </View>
+            <Text style={styles.FP}> Forgote Password</Text>
+            <View style={styles.buttom1}>
+                <Pressable style={styles.button}><Text style={styles.buttomSI}>Sign in</Text></Pressable>
+            </View>
+            <View  style={styles.textDS}>
+                <Text style={styles.textDon}>Don’t have an account?</Text>
+                <Text style={styles.textSU}> Sign Up</Text>
+            </View>
+
+            <View style={styles.line}>
+
+                <Image source={require('../../image/lineDuoi.png')}
+                    style={styles.lineDuoi}
+                    resizeMode='cover' ></Image>
+
+            </View>
+        </View>
+    )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+
+    line:{
+        top:30,
+        alignItems:'center',
+    },
+    textSU:{
+        color:'#FF5E00',
+    },
+    textDon:{},
+    textDS:{
+        top:1,
+        flexDirection:'row',
+        left:100,
+    },
+
+    buttomSI: {
+        left: '41.87%',
+        fontWeight: '700',
+        fontSize: 20,
+        lineHeight: 22,
+        letterSpacing: 0.41,
+        color: 'white',
+    },
+    buttom1: {
+        alignItems: 'center'
+    },
+
+    button: {
+        width: 343,
+        height: 48,
+        bottom: 5,
+        backgroundColor: '#FF5E00',
+        borderRadius: 30,
+        padding: 12,
+
+    },
+
+    FP: {
+        left: '60.50 %',
+        bottom: 68,
+        fontWeight: '400',
+        fontSize: 14,
+        lineHeight: 17,
+        letterSpacing: 0.41,
+        color: '#FF5E00'
+
+    },
+
+
+
+    eyes: {
+        top: 12,
+        left: 238,
+
+    },
+
+    password: {
+
+        bottom: 84,
+        width: 349,
+        height: 48,
+        left: 16,
+        backgroundColor: '#F3F3F3',
+        flexDirection: 'row'
+
+    },
+    inPassword: {
+        color: '#AC8E71'
+    },
+
+    numberPhone: {
+        width: 283,
+        height: 48,
+        backgroundColor: '#F3F3F3',
+        left: 22,
+        color: '#AC8E71'
+    },
+
+    tenXuong: {
+        top: 22,
+        left: 20
+    },
+
+    co: {
+        top: 12,
+        left: 10
+    },
+    icon: {
+        bottom: 90,
+        width: 343,
+        height: 48,
+        flexDirection: 'row',
+        left: 16,
+        backgroundColor: '#F3F3F3'
+    },
+    detail: {
+        left: '4.27%',
+        right: '4.27%',
+        bottom: 110,
+        fontWeight: '350',
+        fontSize: 22,
+        lineHeight: 26,
+        letterSpacing: 0.41,
+        color: '#6D3805'
+    },
+    image4: {
+        bottom: 70
+    },
+    textSI: {
+        width: 78,
+        height: 41,
+        left: 149,
+        top: 14,
+        fontWeight: '700',
+        fontSize: 24,
+        lineHeight: 41,
+        letterSpacing: 0.41,
+        color: '#FF5E00'
+    },
+
+
+
+    imageTren: {
+        width: 8.49,
+        height: 14,
+        left: 20,
+        top: 10
+    },
+
+    container: {
+        flex: 1,
+    },
+
+
+})
