9c172bb3f4e2e036524a234779018f9e449c34eb
QLam06 <lamvip06192003@gmail.com>
2023-02-24 09:14:50 +0700

    xay dung app 2

--- Grocery_app/App.js
+++ Grocery_app/App.js
@@ -1,17 +1,21 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {Text, View} from 'react-native';
 import ChangePassWord from './src/ChangePassWord';
 import EditProfile from './src/EditProfile';
 import ListMyCard from './src/ListMyCard';
 
+const Tab = createBottomTabNavigator();
+
 const App = () => {
   return (
-    <View>
-      {/* <ChangePassWord /> */}
-      {/* <NewCard /> */}
-      {/* <EditProfile /> */}
-      <ListMyCard />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Screen name="ListMyCard" component={ListMyCard} />
+        <Tab.Screen name="EditProfile" component={EditProfile} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
