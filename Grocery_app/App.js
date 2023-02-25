4484ef7914cd05f4fee98fc6b539adb1d2097d89
theking250902 <buivanvietx5hongloc@gmail.com>
2023-02-25 15:12:26 +0700

    t them vao compoment

--- Grocery_app/App.js
+++ Grocery_app/App.js
@@ -2,9 +2,9 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {Text, View} from 'react-native';
-import ChangePassWord from './src/ChangePassWord';
-import EditProfile from './src/EditProfile';
-import ListMyCard from './src/ListMyCard';
+import ChangePassWord from './src/compoment03/ChangePassWord';
+import EditProfile from './src/compoment03/EditProfile';
+import ListMyCard from './src/compoment03/ListMyCard';
 
 const Tab = createBottomTabNavigator();
 
