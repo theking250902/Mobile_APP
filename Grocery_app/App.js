894698c0fa6a2c405b9076b4a63d6288c22adc95
theking250902 <buivanvietx5hongloc@gmail.com>
2023-02-25 15:49:40 +0700

    v

--- Grocery_app/App.js
+++ Grocery_app/App.js
@@ -1,22 +1,19 @@
-import { View, Text } from 'react-native'
+
 import React from 'react'
-import Welcome from './src/component1/Welcome'
-import Address from './src/component1/Address'
+import { View } from 'react-native'
 import Orders from './src/component1/Orders'
-import Ongoing from './src/component1/Ongoing'
-import EditAddress from './src/component1/EditAddress'
 import Payment01 from './src/component1/Payment01'
 
 
- 
 
 
 const App = () => {
   return (
     <View>
-    <Payment01/>
+      <Payment01/>
     </View>
   )
 }
 
 export default App
+
