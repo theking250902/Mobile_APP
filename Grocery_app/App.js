d61c924e8367c2a983d74be8f1aaf00ca76897d6
theking250902 <buivanvietx5hongloc@gmail.com>
2023-03-10 23:10:55 +0700

    profile

--- Grocery_app/App.js
+++ Grocery_app/App.js
@@ -18,23 +18,26 @@ import Login from './src/screens/Login';
 import Register1 from './src/screens/Register1';
 import Register2 from './src/screens/Register2';
 import Register3 from './src/screens/Register3';
+import Screen3 from './src/components/app/screens/Screen3';
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
-   <NavigationContainer>
-    {/* <Navigation/> */}
-    <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown:false}}>
+//    <NavigationContainer>
+//     {/* <Navigation/> */}
+//     <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown:false}}>
  
-    <Stack.Screen name="Login" component={Login} />
-    <Stack.Screen name="Register1" component={Register1} />
-    <Stack.Screen name="Register2" component={Register2} />
-    <Stack.Screen name="Register3" component={Register3} />
-    <Stack.Screen name="Navigation" component={Navigation} />
+//     <Stack.Screen name="Login" component={Login} />
+//     <Stack.Screen name="Register1" component={Register1} />
+//     <Stack.Screen name="Register2" component={Register2} />
+//     <Stack.Screen name="Register3" component={Register3} />
+//     <Stack.Screen name="Navigation" component={Navigation} />
 
-    </Stack.Navigator>
-   </NavigationContainer>
-
+//     </Stack.Navigator>
+//    </NavigationContainer>
+<View>
+    <Screen3/>
+</View>
   )
 }
 
